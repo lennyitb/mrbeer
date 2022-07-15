153f0e7d11149132154a8a4b4817ae3039d717b9
Leonard Phelan <lennyphelan@gmail.com>
2022-07-15 00:35:57 -0400

    fix small update price bug

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -8,16 +8,10 @@ class Admin::DashboardController < ApplicationController
 		require_admin!
 		# puts "form submitted:" << params[:new_price]
 		finput = params[:new_price]
-		if $beerprice.value = finput
+		if $beerprice.set finput
 			redirect_to admin_path, notice: 'Updated price!'
 		else
 			redirect_to change_price_path, notice: 'Invalid price entered!'
 		end
-		# if finput =~ /^\d{1,4}\.?\d{0,2}$/
-		# 	File.open('beerprice.txt', 'w') { |file| file.write "%0.2f" % [finput] }
-		# 	redirect_to admin_path, notice: 'Updated price!'
-		# else
-		# 	redirect_to change_price_path, notice: 'Invalid price entered!'
-		# end
 	end
 end
